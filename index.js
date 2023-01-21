2c344dbb0a1d22eb5b2764343891637f5dd5f04d
Malcolm <milkman96@outlook.com>
2023-01-21 09:20:09 -0500

    test

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get('/sattrack', async (request,response) => {
     let velocity, altitude, satData, sat_data;
     fetchData();
     async function fetchData(){
-        const API_KEY = process.env.API_KEY;
+        const API_KEY = `EZTRJU-4TZN3V-E6CBFN-4WA8`;
         const n2yo_url = `https://api.n2yo.com/rest/v1/satellite/positions/25544/41.702/-76.014/1/1/&apiKey=${API_KEY}`;
         const n2yo_response = await fetch(n2yo_url);
         const sat_data = await n2yo_response.json();
